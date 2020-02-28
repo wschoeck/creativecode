07ed05dfe4be43d1249a96ea744088ed565a8495
wschoeck <wschoeck@bluewin.ch>
2020-02-28 12:50:26 +0100

    initial commit

---
+++ main_js/main.js
@@ -0,0 +1,22 @@
+let p2 = document.getElementById("p2");
+
+/* let colororange = function() {
+
+console.log("Hallo");
+
+p2.style.backgroundColor = "orange";
+
+}
+
+p2.addEventListener("click", colororange);
+
+*/
+
+let typeone = document.getElementById("typeone");
+
+let toggletype = function() {
+  console.log("Hallo");
+  typeone.classList.toggle("toogletype");
+}
+
+typeone.addEventListener("click", toggletype);
